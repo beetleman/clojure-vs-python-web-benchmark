374cd675bc381ec1c41f944f3618b87ce821688b
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2017-10-05 23:29:44 +0200

    yada delay depends of query param

--- yada-app/src/yada_app/routes.clj
+++ yada-app/src/yada_app/routes.clj
@@ -1,17 +1,26 @@
 (ns yada-app.routes
   (:require [yada.yada :as yada]
+            [schema.core :as s]
             [manifold.deferred :as d]))
 
 
 
-(defn hello-handler [ctx]
-  (let [result (d/future
-                 (Thread/sleep 1000)
+(defn hello-response [ctx]
+  (let [delay  (get-in ctx [:parameters :query :delay])
+        result (d/future
+                 (Thread/sleep delay)
                  "hello world")]
     result))
+
+(def hello-resource
+  (yada/resource
+   {:methods {:get
+              {:parameters {:query {:delay s/Int}}
+               :produces   {:media-type "text/plain"}
+               :response   hello-response}}}))
 
 (defn routes [base]
   [base
    [
-    ["" (yada/as-resource hello-handler)]
+    ["" hello-resource]
     [true (yada/as-resource nil)]]])
