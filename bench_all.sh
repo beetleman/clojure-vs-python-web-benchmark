a2a5e237ffb4be0c9c58efb0bb925d74bd017474
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2017-10-07 22:18:01 +0200

    add header "keepalive""

--- bench_all.sh
+++ bench_all.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+run_wrk=wrk -t12 -c1000 -d30s -H "Connection: keep-alive"
+
 for name in `ls -d *-app`
 do
 
@@ -16,8 +18,7 @@ do
     sleep 10
 
     echo "Warm up..."
-    wrk -t12 -c400 -d10s http://localhost:8080/?delay=0
-    wrk -t12 -c400 -d10s http://localhost:8080/?delay=100
+    $run_wrk http://localhost:8080/?delay=1
 
     for factor in 0 0.2 0.4 0.6 0.8 1 1.5 2 2.5 3
     do
@@ -25,8 +26,7 @@ do
         delay=`echo $factor*100/1 | bc`
         echo "benchmark [$delay ms]:"| tee -a ./artifacts/results.log
 
-        TITLE=$name DELAY=$delay wrk \
-             -t12 -c400 -d60s \
+        TITLE=$name DELAY=$delay $run_wrk
              -s ./report.lua \
              http://localhost:8080/?delay=$delay | tee -a ./artifacts/results.log
 
